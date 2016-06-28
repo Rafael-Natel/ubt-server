6f8fd555a3bab3df341d8f47a7556a7fdea5dd5a
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-06-27 22:47:32 -0300

    basic server

--- Server.hx
+++ Server.hx
@@ -4,7 +4,7 @@
 //
 //	Arquivo Server.hx
 //	Implementação básica de um servidor TCP/IP
-//	Adaptado do código fonte original em: 
+//	Adaptado do código fonte original em:
 //	"Writing a Client/Server Tutorial"
 // 	URL: http://old.haxe.org/doc/neko/client_server
 // 	Última atualização em 16/09/2015
@@ -22,41 +22,48 @@ enum State{
 
 class Player {
     var _name:String;
-    
-    override function new (name:String) {
+	var _character:String;
+
+    public function new (name:String) {
         _name = name;
     }
+
+	public function Name():String {
+		return _name;
+	}
+
+	public function setCharacter(name:String) {
+		_character = name;
+	}
 }
 
 class Server {
-    private var _player1:Player;
-    private var _player2:Player;
-    private var _state:State;
-    
     static function main() {
+		var _player1:Player = null;
+		var _player2:Player = null;
+		var _state:State;
 
-    // instancia novo socket s
-    var s = new sys.net.Socket();
+		var s = new sys.net.Socket();
 
-    // usa o método BIND para começar
-    // a escuta na porta e IPs 
-    // especificados no parâmetro
-    var host = "0.0.0.0";
-    var port = 5000;
-        
-    s.bind(new sys.net.Host("host"), port);
-        
+		// usa o método BIND para começar
+		// a escuta na porta e IPs
+		// especificados no parâmetro
+		var host = "127.0.0.1";
+		var port = 5000;
+
+		s.bind(new sys.net.Host(host), port);
+
         s.listen(1);
 //      $host:$port'
         trace("Starting server at ...");
-        
+
         var _minhaconexao: sys.net.Socket = s.accept();
-        
+
         trace("Client connected");
-       
+
         _state = CONNECT;
         var curPlayer:Player = null;
-        
+
         if(_player1 == null) {
             _player1 = new Player("player1");
             curPlayer = _player1;
@@ -66,22 +73,29 @@ class Server {
         }
 
         while(true) {
-            
-            switch _state {
-                    case CONNECT:
-                    
-                    
+
+            switch (_state) {
+				case CONNECT:
+					var data:String = '{"player": "' + curPlayer.Name() + '", "status": "OK"}';
+
+					_minhaconexao.write(data+"\n");
+					_state = SELECT;
+				case SELECT:
+					trace("waiting for select");
+					var data:String = _minhaconexao.input.readLine();
+
+					var selectObject:Dynamic = Json.parse(data);
+
+					trace("received:", selectObject.character);
+					curPlayer.SetCharacter(selectObject.character);
+					_state = SYNC;
+
+				case SYNC:
+				case PLAY:
             }
 
-            var _PersonagemData: String = '{"Type":"1","Life":"'+_life+'","Mana":"456","Items":["Potion","Sword","Shield","Helm","300 Gold"]}';                
-                
-            // Envia string 
-            _minhaconexao.write(_PersonagemData + "\n");
-
-            // Uma pausa anti-flood, para fins
-            // de demonstrção somente.
             Sys.sleep(4);
 
-            }
+		}
     }
 }
